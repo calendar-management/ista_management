091724b72513cb1f5367f0c9403657920fd2bf95
Hassanezz11 <hassan.ezz319@gmail.com>
2025-03-05 12:23:53 +0000

    calendar update

---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+use App\Http\Controllers\FormController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('index');
+});
+
+Route::get('/admin_dashboard',function(){
+    return view("admin.dashboard");
+});
+Route::get('/formateur_dashboard',function(){
+    return view("formateur.dashboard");
+});
+Route::get('/formateur_calendar',function(){
+    return view("formateur.calendar");
+});
