7e494fff4593137bc25faa1fa3dfa8b71c1df861
abdessadek <abdessadekbirama1@gmail.com>
2025-03-07 14:46:04 +0100

    test

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\AdministrateurController;
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
+use App\Http\Controllers\FormateurController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -14,18 +17,42 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
+
 Route::get('/', function () {
     return redirect()->route('dashboard');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+
 
 Route::middleware('auth')->group(function () {
+    Route::post('/logout',[AuthenticatedSessionController::class,'destroy']);
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::view('/dashboard','admin.dashboard')->name('dashboard');
+    Route::view('/sup_adm_dashboard',"supadmin.dashboard");
+    Route::view('/adm_dashboard',"admin.dashboard");    
+
+    Route::get('/gestion_formateur', [FormateurController::class, 'index']);
+
+    Route::get('/gestion_adm', [AdministrateurController::class, 'index']);
+
+
+
+    Route::view('/add_admin','supadmin.add_adm')->name('add_admin');
+
+    Route::post('/add_admin',[AdministrateurController::class, 'add']);
+    Route::get('/edit_adm/{id}',[AdministrateurController::class, 'edit'])->name('edit_adm');
+    Route::put('/edit_adm/{id}',[AdministrateurController::class, 'update'])->name("update_admin");
+    Route::delete('/delete_admin/{id}',[AdministrateurController::class, 'delete'])->name("delete_admin");
+
 });
 
 require __DIR__.'/auth.php';
+
+
+
+
+
+
