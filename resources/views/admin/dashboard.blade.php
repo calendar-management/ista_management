62fe7c5d9db6a421848de277fd1c364dd1d429a2
abdessadek <abdessadekbirama1@gmail.com>
2025-03-10 12:23:52 +0100

    fix logout

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -133,10 +133,13 @@
                                 </a>
 
                                 <div class="dropdown-divider"></div>
-                                <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
+                                <form action="{{route('logout')}}" method="post">
+                                    @csrf
+                                    <button class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
                                     <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
                                     Logout
-                                </a>
+                                    </button>
+                                </form>
                             </div>
                         </li>
 
