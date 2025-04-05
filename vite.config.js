c119d006ef983953f7668e5bc7da36e8e02320b4
abdessadek <abdessadekbirama1@gmail.com>
2025-04-05 16:20:00 +0200

    test

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,9 @@ export default defineConfig({
             input: [
                 'resources/css/app.css',
                 'resources/js/app.js',
+                // 'resources/js/app.jsx',
+                'resources/js/calendar.js',
+                'resources/js/vacances.js',
             ],
             refresh: true,
         }),
