f8180211de83e075abca24e23eb68cd12c5c7dd5
abdessadek <abdessadekbirama1@gmail.com>
2025-04-09 14:23:06 +0200

    hllo

--- resources/views/components/bar.blade.php
+++ resources/views/components/bar.blade.php
@@ -13,11 +13,11 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
 
-    <link href='assets/css/fullcalendar.css' rel='stylesheet' />
-    <link href='assets/css/fullcalendar.print.css' rel='stylesheet' media='print' />
-    <script src='assets/js/jquery-1.10.2.js' type="text/javascript"></script>
-    <script src='assets/js/jquery-ui.custom.min.js' type="text/javascript"></script>
-    <script src='assets/js/fullcalendar.js' type="text/javascript"></script>
+    <link href='{{asset('assets/css/fullcalendar.css')}}' rel='stylesheet' />
+    <link href='{{asset('assets/css/fullcalendar.print.css')}}' rel='stylesheet' media='print' />
+    <script src='{{asset('assets/js/jquery-1.10.2.js')}}' type="text/javascript"></script>
+    <script src='{{asset('assets/js/jquery-ui.custom.min.js')}}' type="text/javascript"></script>
+    <script src='{{asset('assets/js/fullcalendar.js')}}' type="text/javascript"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.4/moment.min.js"></script>
     <style>
         .logout {
